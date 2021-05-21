2c07fb072bca318181017125d5b5958610b7dd0a
HasymiMuhamad <muhamad.hilman@myinfosys.net>
2021-05-21 16:02:11 +0700

    restructure directory and component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,6 @@
+import MyAppBasic from './layout/index'
 import './App.css';
 
-
-const MyAppBasic = ()=>{
-  return (
-    <div className="main-app">
-        <h1>My Github App</h1>
-        <div className="header-search">
-            <input className="input-search" type="text" id="search" name="search" placeholder="Type a Repository name" />
-            <button>Search</button>
-        </div>
-        <div className="main-list">
-          <div className="list-item">
-            <div className="item">
-              <p className="item-title">AJV-Validate</p>
-              <p className="item-desc">Use AJV to validate data</p>
-            </div>
-
-            <div className="item">
-              <p className="item-title">AJV-Validate</p>
-              <p className="item-desc">Use AJV to validate data</p>
-            </div>
-
-           
-
-            
-
-            
-          </div>
-        </div>
-    </div>
-  )
-}
 
 function App() {
   return (
---
+++ src/layout/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+
+
+const MyAppBasic = ()=>{
+    return (
+      <div className="main-app">
+          <h1>My Github App</h1>
+          <div className="header-search">
+              <input className="input-search" type="text" id="search" name="search" placeholder="Type a Repository name" />
+              <button>Search</button>
+          </div>
+          <div className="main-list">
+            <div className="list-item">
+              <div className="item">
+                <p className="item-title">AJV-Validate</p>
+                <p className="item-desc">Use AJV to validate data</p>
+              </div>
+  
+              <div className="item">
+                <p className="item-title">AJV-Validate</p>
+                <p className="item-desc">Use AJV to validate data</p>
+              </div>
+  
+            </div>
+          </div>
+      </div>
+    )
+  }
+
+export default MyAppBasic;
