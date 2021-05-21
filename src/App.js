99c20ea1dfe10dcde1a22c7b2f4d0cb62113a88a
HasymiMuhamad <muhamad.hilman@myinfosys.net>
2021-05-21 15:54:35 +0700

    create UI to show list of repositories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const MyAppBasic = ()=>{
+  return (
+    <div className="main-app">
+        <h1>My Github App</h1>
+        <div className="header-search">
+            <input className="input-search" type="text" id="search" name="search" placeholder="Type a Repository name" />
+            <button>Search</button>
+        </div>
+        <div className="main-list">
+          <div className="list-item">
+            <div className="item">
+              <p className="item-title">AJV-Validate</p>
+              <p className="item-desc">Use AJV to validate data</p>
+            </div>
+
+            <div className="item">
+              <p className="item-title">AJV-Validate</p>
+              <p className="item-desc">Use AJV to validate data</p>
+            </div>
+
+           
+
+            
+
+            
+          </div>
+        </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyAppBasic />
     </div>
   );
 }
